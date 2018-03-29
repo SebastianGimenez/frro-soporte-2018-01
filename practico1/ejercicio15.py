4b373c3333626aeb534295b6c95d04b3e8430e16
Favio <favio@syloper.com>
2018-03-29 11:01:02 -0300

    limpieza

--- practico1/ejercicio15.py
+++
@@ -1,9 +0,0 @@
-
-lista=[]
-a=(input("ingrese numero"))
-while a!= "fin":
-    lista.append(a)
-    a=(input("ingrese numero"))
-if len(lista)>0:
-    print(max(lista))
-    print(min(lista))
--- practico1/ejercicio7.py
+++
@@ -1,7 +0,0 @@
-s=(input("ingrese cadena"))
-
-def esPalindromo(a):
-    print(a==(a[::-1]))
-    return(a==a[::-1])
-
-esPalindromo(s)
